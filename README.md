9f186d679a998818ba4c119b76a54141a82fbd2c
Eduardo Paul <ecpaul@protonmail.com>
2020-09-10 17:34:34 -0300

    Advanced the analysis.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# Satisfação dos clientes do Santander
+# Satisfação dos clientes do Santander.
 Projeto da Formação Cientista de Dados da [Data Science Academy](https://www.datascienceacademy.com.br/), inspirado por uma competição do [Kaggle](https://www.kaggle.com/c/santander-customer-satisfaction/).
 
-A variável "ID" identifica cada cliente, e a variável "TARGET" indica a sua satisfação, onde 0 é "satisfeito" e 1 é "insatisfeito."
+## Descrição do problema e objetivos.
+Este conjunto apresenta dados de 76020 clientes, para os quais são retratadas 369 variáveis. Como esses dados são relativos a clientes reais, toda a informação foi anonimizada, de forma que os nomes das variáveis, assim como seus valores, pouco ajudam a entender o problema.
+
+Neste conjunto de dados, a variável "ID" identifica cada cliente, e a variável "TARGET" indica a sua satisfação, onde 0 é "satisfeito" e 1 é "insatisfeito." O objetivo aqui é detectar corretamente a insatisfação do cliente, que definimos então como a classe positiva.
+
+## Considerações.
+Explorando os dados, é possível perceber que o problema é altamente desbalanceado, tendo apenas cerca de 4% dos clientes classificados como insatisfeitos. Esse desbalancemento traz dificuldades para a obtenção de um modelo de classificação. Por exemplo, um modelo que preveja que nunca um cliente estará insatisfeito terá 96% de acurácia. Apesar de este valor num primeiro momento sugerir um modelo de sucesso, o fato de ele nunca ser capaz de detectar a insatisfação de um cliente torna-o inútil para o nosso propósito.
+
+Uma medida mais interessante seria a sensibilidade, que mede a taxa de acerto entre os casos positivos (quantos dos clientes insatisfeitos foram corretamente classificados). Em outras palavras, é uma medida de quanto o modelo é _sensível_, ou o quanto ele é capaz de detectar, a insatisfação.
+
+De maneira complementar à sensibilidade, também podemos definir a especificidade. Esta métrica resume a taxa de acerto do modelo nos casos negativos (clientes satisfeitos). O modelo é bastante _específico_ quando ele detecta apenas os casos que são verdadeiramente positivos. Em outras palavras, ele é capaz de "enxergar" _especificamente_ a condição procurada, não se deixando enganar por distrações.
+
+Para levar em consideração as duas métricas descritas, podemos utilizar a média geométrica.
--- eda.py
+++ eda.py
@@ -1,6 +1,10 @@
 #%%
 from pathlib import Path
 import pandas as pd
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+import training_tools as tt
 
 #%%
 data = pd.read_csv(
@@ -8,16 +12,71 @@ data = pd.read_csv(
 )
 
 #%%
-data.info()
-# O conjunto de dados contém 76_020 linhas e 371 colunas, sendo que todas as colunas são do tipo float64 ou int64.
+# A classificação dos clientes é dada pela variável TARGET.
+X = data.drop(columns=['TARGET'])
+y = data['TARGET']
 
 #%%
-print(data['ID'].describe())
-data['ID'].hist()
-# É interessante notar que os IDs não coincidem com os índices da tabela, porém estão uniformemente distribuídos. Isso provavelmente quer dizer que não são relevantes.
+X.info()
+# O conjunto de dados contém 76_020 linhas e 370 colunas, sendo que todas as colunas são do tipo float64 (111) ou int64 (259).
 
 #%%
-print(data['TARGET'].value_counts())
-# Podemos ver que o conjunto é altamente desbalanceado, tendo apenas cerca de 3008 / (73_012 + 3008) ~ 4% de clientes satisfeitos.
+# Os IDs são todos distintos (como era de se esperar) e não coincidem com o índice da tabela.
+print(f"Número de IDs distintos: {X['ID'].nunique()}")
+
+coinciding_indices = sum(X['ID'] == X.index)
+print(f'Número de IDs que coincidem com o índice do data frame: {coinciding_indices}')
+
+#%%
+# Os índices estão uniformemente distribuídos. Isso provavelmente quer dizer que não são relevantes e podem ser descartados.
+X['ID'].hist()
+X = X.drop(columns=['ID'])
+
+#%%
+# Podemos ver que o conjunto é altamente desbalanceado, tendo apenas cerca de 3008 / 76_020 ~ 4% de clientes insatisfeitos. Com esse nível de desbalanceamento, não será adequado utilizar a acurácia como métrica.
+print(
+    f"Número de clientes em cada classe:",
+    y.value_counts(),
+    sep='\n',
+)
+
+#%%
+# As variáveis desta tabela não tem nomes claros devido à anonimização dos dados. Isso faz com que qualquer intuição a respeito delas seja muito difícil.
+print(data.columns)
+
+#%%
+# Para ter alguma intuição melhor sobre o conjunto de dados, nós podemos verificar a existência de variáveis que apresentem valores constantes para todos os clientes. Essas variáveis podem ser facilmente descartadas, já que não tem poder preditivo.
+const_var = VarianceThreshold()
+data01 = const_var.fit_transform(data)
+num_elim_var = data.shape[1] - data01.shape[1]
+print(f'Número de variáveis eliminadas: {num_elim_var}')
+print(f'Número de variáveis restantes: {data01.shape[1]}')
+
+#%%
+# Como ainda resta um número muito grande de variáveis (337) e essas variáveis são de difícil interpretação, podemos buscar quais delas são individualmente relevantes para a tarefa de classificação. Isso pode ser feito facilmente com uma árvore de decisão, utilizando as variávies uma a uma. Como as classes são desbalanceadas, o modelo não vai conseguir se ajustar bem naturalmente. Por isso devemos especificar que as classes devem receber pesos de maneira inversamente proporcional à sua predominância.
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=101)
+
+tree = DecisionTreeClassifier(
+    class_weight='balanced',
+    random_state=102,
+)
+
+results = pd.DataFrame(columns=['feature', 'sensitivity', 'specificity', 'roc_auc'])
+for feature in X_train.columns:
+    # Treino.
+    tree.fit(X_train[[feature]], y_train)
+    # Predição.
+    y_pred = tree.predict(X_test[[feature]])
+    # Dicionário com as métricas.
+    report = tt.report(y_test, y_pred)
+    # Acrescentando o nome da variável ao dicionário.
+    report['feature'] = feature
+
+    new_row = pd.DataFrame(report)
+    results = results.append(new_row, ignore_index=True)
+else:
+    results = results.sort_values(by='roc_auc', ascending=False)
+
+print(results)
 
 #%%
